8665f22ad6b460dbc2c01127b909cb5370496640
Evgeny Timofeev <repeatitplease@gmail.com>
2022-05-22 13:26:52 +0300

    >

--- RXSwiftTrainProject/ViewController.swift
+++ RXSwiftTrainProject/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var names = BehaviorRelay(value: ["Name"])
+    
+    var bag = DisposeBag() //позволяет избежать Retain cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        names.asObservable()
+            .throttle(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+            .filter({ value in
+                value.count > 1 // вот тут смотреть!!!
+            })
+            .map({ value in
+                value.joined(separator: ",")
+            })
+            .subscribe(onNext: { [weak self] value in
+            self?.label.text = value
+        }).disposed(by: bag)
+        
+
+        names.accept(["Alex", "Maria", "Bob"])
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute:  { [weak self] in
+            self?.names.accept(["Anatoliy"]) //данный блок кода выполнен не будет, тк мы подписаны на условие, которое описывает обновление лейбла в том случае, если мы имеем более одного элемента в массиве (смотреть выше)
+        })
+        
     }
-
-
 }
 
